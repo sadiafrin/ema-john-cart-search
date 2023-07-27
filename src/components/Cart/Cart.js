71501b408279dd3e934f4bb3615b4eb37cea030d
sadiafrin <sadiafrin328@gmail.com>
2023-07-27 07:47:20 +0600

    added cart component and calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,7 +20,7 @@ const Cart = (props) => {
              <p>Select items: {cart.length}</p>
              <p>Total price: $ {total}</p>
               <p>Total shipping: ${shipping}</p>
-              <p>Tax: ${tax}</p>
+              <p>Tax: ${tax.toFixed(2)}</p>
               <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
