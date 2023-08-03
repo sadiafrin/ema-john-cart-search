11e88b5703af163f119b59829a80f76e8f2590fe
sadiafrin <sadiafrin328@gmail.com>
2023-08-03 09:15:33 +0600

    cart section added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
 
 const Orders = () => {
-    const [products, setProducts] =useProducts();
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h1>This is orders: {products.length}</h1>
+            <p>Cart has: {cart.length}</p>
         </div>
     );
 };
